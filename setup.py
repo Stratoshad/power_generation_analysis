10c903e28c0981e8a5e11f87e331e5a268831b62
Stratos <stratosh@hotmail.com>
2020-06-30 19:50:52 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project analysing energy data from EU countries',
+    author='Stratoshad',
+    license='MIT',
+)
